6daa1f9fd05f5c1e3c867b0a01de87b01294a2ee
saad <saadchoubi14@gmail.com>
2025-05-03 16:25:12 +0100

    test

---
+++ src/main/java/org/example/chatapp/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package org.example.chatapp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private String receiver;
+    private MessageType type;
+}
